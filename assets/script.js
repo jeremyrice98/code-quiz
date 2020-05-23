ea61556460b33c2538ec6ff187efec3500e76904
jeremyrice98 <ricefamily1003@gmail.com>
2020-05-23 08:33:26 -0600

    add counter

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,21 @@
+function countdown() {
+    var timeLeft = 5;
+    var timeInterval = setInterval(function() {
+        countdownEl.textContent = timeLeft;
+        timeLeft--;
+
+        if (timeLeft === 0) {
+            countdownEl.textContent = "Times up!"
+                //clearInterval(timeInterval);
+        }
+
+    }, 1000)
+}
 
 
 
+
+
+
+
+countdown();
